b1640dcc8c48f41cefd7a2d9eb41e445ba1bc643
hoyong2007 <hoyong2007@naver.com>
2021-08-11 07:30:41 -0700

    tmp upload

---
+++ get_all.py
@@ -0,0 +1,182 @@
+import util
+import json
+import os
+import sys
+from pprint import pprint
+import r2pipe
+from subprocess import check_output
+import time
+
+
+callable_functions = dict()
+syscall_file = ''
+System_map_file = ''
+
+syscalls = ''
+system_map = ''
+version = ''
+
+def get_pipe(vmlinux_file, version):
+    plist = check_output(['r2', '-p']).strip().split('\n')
+    project = 'project_%s' % (version.replace('.','_'))
+    if project in plist and False:
+        print('Open %s' % project)
+        print(vmlinux_file)
+        r = r2pipe.open('vmlinux', ['-p', project])
+    else:
+        print('Open new')
+        r = r2pipe.open('vmlinux')
+        r.cmd('aaa')
+    return r
+
+def init(kpath):
+    # parse basic information
+    syscall_file = os.path.join(kpath, 'arch/x86/entry/syscalls/syscall_64.tbl')
+    System_map_file = os.path.join(kpath, 'System.map')
+    vmlinux_file = os.path.join(kpath, 'vmlinux')
+
+    system_map = get_system_map(System_map_file)
+    syscall_list = get_syscall_list(syscall_file, system_map)
+    version = kpath.split('linux-')[1].replace('/','')
+
+    # analyze with radare and get handle
+    cwd = os.getcwd()
+    os.chdir(kpath)
+    r = get_pipe(vmlinux_file, version)
+    os.chdir(cwd)
+
+    # make result directoy
+    logdir = os.path.join(cwd, 'result-%s' % version)
+    if not os.path.isdir(logdir):
+        os.mkdir(logdir)
+
+    return r, version, system_map, syscall_list
+
+def get_ref(data, function, address):
+    imports = []
+    for d in data:
+        f = d['name'].lower().replace('dbg.','')
+        f = f.replace('sym.','')
+        f = f.replace('obj.','')
+        f = f.replace('loc.','')
+        
+        if f == address.replace('0x',''): # find with address
+            imports = d['imports']
+            break
+        if f == function:   # find with function name
+            imports = d['imports']
+            break
+
+    return imports
+
+depth_function = ''
+callable_functions = dict()
+def extract_callable_function(data, function, address, depth):
+    global depth_function, callable_functions
+
+    depth_function += '  ' * depth
+    depth_function += '%s\n' % func
+
+    refs = get_ref(data, function, address)
+    for name in refs:
+        if 'unk.' in name:  # skip unknown
+            continue
+
+        f = name.lower().replace('dbg.','')
+        f = f.replace('sym.','')
+        f = f.replace('obj.','')
+        f = f.replace('loc.','')
+        if 'fcn.' in f:
+            addr = '0x' + f.replace('fcn.','')
+        else:
+            addr = name2addr(f)
+
+        if addr == '' or is_func(addr) == False:
+            continue
+        
+        func = addr2name(addr)
+        if func not in callable_functions:
+            callable_functions[func] = addr
+            extract_callable_function(data, func, addr, depth+1)
+
+    return callable_functions
+
+
+def step1(system_map):
+    print("start analyzing each functions...")
+    sysmap_sorted = sorted(system_map.items(), key=lambda sys:int(sys[1]['addr'],16), reverse=False)
+    end_addr = '0'
+    for sys in sysmap_sorted:
+        name = sys[0]
+        addr = sys[1]['addr']
+        if is_func(addr) == False:
+            continue
+        if int(addr,16) < int(end_addr, 16):    # skip if addr is already analyzed
+            continue
+            
+        r.cmd('s %s' % addr)
+        r.cmd('af')
+        bb_info = get_basicblock(r, {'name':name, 'addr':addr})
+        if '0xffffff' not in bb_info:   # Cannot find function
+            continue
+        #print bb_info
+        end_addr = '0'
+        for bb in bb_info.strip().split('\n')[1:]:
+            bb = bb.split(':')
+            start = bb[0]
+            bb = bb[1].split(' ')
+            end = bb[0]
+            size = bb[1]
+            if '0xffffff' not in end:
+                continue
+            if start == end_addr or end_addr == '0':
+                end_addr = end
+            elif start[:-2] == end_addr[:-2]:
+                end_addr = end
+                print 'not same but in range'
+    
+    r.cmd('agCj > %s/out.json' % logdir)
+    print('extract out.json successs')
+
+
+def step2(result_path, data, system_map, syscall_list):
+    global depth_function, callable_functions
+
+    for func in syscall_list:
+        addr = name2addr(func)
+        if addr != None:
+            callable_function[func] = addr
+            extract_callable_function(data, func, addr, 0)
+
+    extracted_callable_with_name = ''
+    for f in callable_function.items():
+        extracted_callable_with_name += '%s\n' % str(f)
+    with open('%s/extracted_callable_with_name.lst' % result_path, 'w') as f:
+        f.write(extracted_callable_with_name)
+    with open('%s/extracted_callable_with_depth.lst' % result_path, 'w') as f:
+        f.write(depth_function)
+
+
+def main():
+    r, version, system_map, syscall_list = init(sys.argv[1])
+    print(version, r)
+
+    cwd = os.getcwd()
+    result_path = os.path.join(cwd, 'result-%s' % version)
+    totalbb_file = os.path.join(result_path, 'total_bb.lst')
+    json_file = os.path.join(result_path, 'out.json')
+    syscall_file = os.path.join(result_path, 'target_syscalls.lst')
+
+    step1(system_map)
+    data = get_json(json_file)
+    step1(result_path, data, system_map, syscall_list)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Usage: python get_all.py <kernel_path>')
+        print('   ex) python get_all.py ../kernel/linux-4.4/')
+        exit(0)
+
+    main()
+
---
+++ util.py
@@ -0,0 +1,142 @@
+import json
+import os
+import sys
+from pprint import pprint
+import r2pipe
+from subprocess import check_output
+import time
+
+system_map = None
+
+def get_json(json_file):
+    with open(json_file) as f:  
+        data = json.load(f)
+    return data
+
+def addr2name(addr, system_map=system_map):
+    for sys in system_map:
+        if system_map[sys]['addr'] == addr:
+            return sys
+    return ''
+
+def name2addr(name, system_map=system_map):
+    if name in system_map:
+        return syscall_map[name]['addr']
+    return ''
+
+
+def is_func(func, system_map=system_map):
+    addr = int(func['addr'], 16)
+
+    stext = int(system_map['_stext']['addr'], 16)
+    etext = int(system_map['_etext']['addr'], 16)
+    if stext <= addr and addr <= etext[version]:
+        return True
+
+    return False
+
+# {'name': {'addr', 'code'}}
+def get_system_map(System_map_file):
+    global system_map
+    with open(System_map_file, 'r') as f:
+        data = f.read().strip()
+
+    result = dict()
+    for line in data.split('\n'):
+        line = line.split()
+        if len(line) != 3:
+            continue
+        addr = '0x' + line[0].strip()   # 0xffff~
+        code = line[1].strip()          # t/T/d ...
+        name = line[2].strip().lower()  # name of functions/symbols, only lowercase
+
+        # for inline functions, add postfix  ex) func_1
+        if name in result and 'sys_' not in name:
+            idx = 1
+            tmp = name
+            while tmp in result:
+                tmp = name + '_%d'%idx
+                idx += 1
+            name = tmp
+                
+        result[name] = {'addr':addr, 'code':code}
+    system_map = result
+    return result
+
+# return list of syscalls which exist in system_map
+def get_syscall_list(syscall_file, system_map):
+    with open(syscall_file, 'r') as f:
+        data = f.read().strip()
+
+    result = list() 
+    for line in data.split('\n'):
+        if line.strip() == '':
+            continue
+
+        line = line.split('\t')
+        if line[0] or line[0][0] != '#': # comment start with '#'
+            continue
+                
+        sys_func = line[-1].strip()
+        if sys_func in system_map.keys() and sys_func not in result:   # original
+            result.append(r)
+
+        sys_func = sys_func.replace('__x64_sys_','sys_')
+        sys_func = sys_func.replace('__x32_sys_','sys_')
+        if sys_func in system_map.keys() and sys_func not in result:   # sys_func
+            result.append(r)
+
+        tmp = 'k' + sys_func
+        if tmp in system_map.keys() and tmp not in result:   # ksys_func
+            result.append(tmp)
+
+        tmp = '__x64_' + sys_func
+        if tmp in system_map.keys() and tmp not in result:   # __x64_sys_func
+            result.append(tmp)
+
+        tmp = '__x32_' + sys_func
+        if tmp in system_map.keys() and tmp not in result:   # __x32_sys_func
+            result.append(tmp)
+
+    return result
+
+# [{'name', 'addr'}, {'name', 'addr'}, ...]
+def get_callable_functions(callable_file):
+    with open(callable_file, 'r') as f:
+        data = f.read().strip().split('\n')
+
+    result = list()
+    for line in data:
+        if line == '':
+            continue
+
+        tmp = eval(line)
+        try:
+            name = tmp[0]
+            addr = tmp[1]
+            if addr == 0 and 'fcn.' in name:
+                addr = name.replace('fcn.','')
+            
+            result.append({'name':name, 'addr':addr})
+        except:
+            continue
+    return result
+
+def get_basicblock(r, info):
+    print(info['addr'])
+    r.cmd('s %s' % info['addr'])
+    raw = r.cmd('afb')
+    #print(info['name'])
+    #print(raw)
+    bb_info = 'Function: %s\n' % info['name']
+    if 'Cannot find function' in raw or raw.strip() == '':
+        bb_info += '%s\n' % raw
+        return bb_info
+
+    for line in raw.strip().split('\n'):
+        tmp = line.split()
+        start = tmp[0]
+        end = tmp[1]
+        size = tmp[3]
+        bb_info += '%s:%s %s\n' % (start, end, size)
+    return bb_info
